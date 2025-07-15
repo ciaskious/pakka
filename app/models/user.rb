34b5a8e187089ce33f34ff58c4caf6054198b6f9
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-15 09:50:41 +0200

    Comment out devise in User model temporarily to fix deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
 
   has_many :trips, dependent: :destroy
   has_many :likes, dependent: :destroy
