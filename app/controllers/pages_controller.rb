519de81156998ef41120804c127285488173e0ea
Anastasia Kaloudi <ciaskious@gmail.com>
2025-07-25 16:01:04 +0200

    fix for users error on heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @my_trips = current_user.trips
-      @community_trips = Trip.where(public: true).where.not(user: current_user)
+      @community_trips = Trip.where(public: true).where.not(user: current_user).includes(:user)
     else
       @my_trips = []
       @community_trips = Trip.where(public: true)
     end
-    @trips = Trip.all
+    @trips = Trip.includes(:user).all
   end
 
   def ui_kit
