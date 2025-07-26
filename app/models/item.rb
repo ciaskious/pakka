e9d10dab03ca301e7886fb72e4d876d367196ec3
Anastasia Kaloudi <ciaskious@gmail.com>
2025-07-26 10:18:48 +0200

    first version of trip checklist logic

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,8 @@ class Item < ApplicationRecord
   has_many :trips, through: :checklist_items
   has_many :likes, through: :checklist_items
 
-  validates :name, presence: true
+  validates :name, :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+
+  CATEGORIES = %w[clothes toiletries tech food documents].freeze
 end
